b145c0912b18492b075179dfad94cd4af9a6e6dc
Youssef <yelabd@purdue.edu>
2017-02-27 23:56:02 -0500

    Added gif

--- Twitter/TwitterClient.swift
+++ Twitter/TwitterClient.swift
@@ -14,7 +14,7 @@ class TwitterClient: BDBOAuth1SessionManager {
     
     static let sharedInstance = TwitterClient(baseURL: URL(string: "https://api.twitter.com")!, consumerKey: "ITAlNcYUNcl1lcDbkI2PeJB53", consumerSecret: "RPRDAzafa2hz5s3LD81uYnBX2cDaSz0CYxGwk99UstOot44yh2")
     
-    func homeTimeLine(success: ([Tweet]) -> (), failure: (Error) -> ()){
+    func homeTimeLine(success: @escaping ([Tweet]) -> (), failure: @escaping (Error) -> ()){
         get("1.1/statuses/home_timeline.json", parameters: nil, progress: nil, success: { (task: URLSessionDataTask,response: Any?) in
             //print(response!)
             let json = JSON(response!).arrayValue
@@ -22,7 +22,6 @@ class TwitterClient: BDBOAuth1SessionManager {
             let tweets = Tweet.tweetsWithArray(jsons: json)
             
             success(tweets)
-            
         }, failure: { (task: URLSessionDataTask?, error: Error) in
             failure(error)
         })
